d44b10bee4ed66112f0f653573ad105489344ef0
gourav <gourav003yadav@gmail.com>
2024-08-28 22:42:59 +0530

    added env

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+require("dotenv").config
 
 app.get("/api/get",(req,res)=>{
     res.send({message:"hello back to nodejs"})
@@ -11,10 +12,10 @@ app.get('/api/get_user_details',(req,res)=>{
             name:"Gourav yadav",
             age:22,
             content:4567890
-        }
+        },env:process.env.Name
     })
 })
 
-app.listen(8000,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("listening to 8000");
 })
