9e7fb1893fc4a486df5cf74c8aa3d025160de8b6
gourav <gourav003yadav@gmail.com>
2024-08-28 21:55:36 +0530

    get user detail

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ app.get("/api/get",(req,res)=>{
     res.send({message:"hello back to nodejs"})
 })
 
+app.get('/api/get_user_details',(req,res)=>{
+    res.send({
+        user:{
+            name:"Gourav yadav",
+            age:22,
+            content:4567890
+        }
+    })
+})
 
 app.listen(8000,()=>{
     console.log("listening to 8000");
